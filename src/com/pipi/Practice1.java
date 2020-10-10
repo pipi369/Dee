67aaddbb28a65856550e20e734dcc00e16fc51f6
guanshengjun@songguo7.com <sgcx251@172-15-69-208.lightspeed.austtx.sbcglobal.net>
2020-10-10 15:09:47 +0800

    LeetCode - 算法1

---
+++ src/com/pipi/Practice1.java
@@ -0,0 +1,48 @@
+package com.pipi;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Practice1 {
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * <p>
+     *  
+     * <p>
+     * 示例:
+     * <p>
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * <p>
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     * <p>
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] results = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            if (map.containsKey(current)) {
+                results[0] = map.get(current);
+                results[1] = i;
+                return results;
+            }
+            map.put(target - current, i);
+        }
+        return results;
+    }
+
+}
